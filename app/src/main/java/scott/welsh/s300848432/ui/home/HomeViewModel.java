f5962ed353001b3eb3121904e97d28083a56f71f
Scott Welsh <scott.welsh@hotmail.com>
2020-10-26 13:42:56 -0400

    Initial Project Files

---
+++ app/src/main/java/scott/welsh/s300848432/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package scott.welsh.s300848432.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
