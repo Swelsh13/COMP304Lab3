cfc747712d829ea52254a72e2f096a5869f4ff21
Scott Welsh <scott.welsh@hotmail.com>
2020-10-27 15:49:00 -0400

    initial back button code

--- app/src/main/java/scott/welsh/s300848432/ScottActivity.java
+++ app/src/main/java/scott/welsh/s300848432/ScottActivity.java
@@ -4,11 +4,16 @@ package scott.welsh.s300848432;
 //student ID: 300848432
 //section number: 002
 
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -30,10 +35,46 @@ public class ScottActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                AlertDialog.Builder dialog=new AlertDialog.Builder(getApplicationContext());
+                dialog.setMessage("Are you sure you want to close");
+                dialog.setTitle("Dialog Box");
+                dialog.setPositiveButton("Exit",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                ScottActivity.super.onBackPressed();
+                            }
+                        });
+                dialog.setNegativeButton("cancel",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"Thank you for staying",Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog alertDialog=dialog.create();
+                alertDialog.show();
+
+            }
+        };
+
+        getOnBackPressedDispatcher().addCallback(callback);
+
+
+
     }
 
     public void clearCanvas(View v){
 
     }
+
+
+
 
 }
